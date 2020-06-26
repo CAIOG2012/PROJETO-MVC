2dd40c0bf04a4981c9534f38be4f5a20e36c5534
CAIOG2012 <67448218+CAIOG2012@users.noreply.github.com>
2020-06-25 22:58:58 -0300

    Add files via upload

---
+++ PROJETOMVC/BLLMVC/BLLMVCA06.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PROJETOMVCA06.DALMVC;
+
+namespace PROJETOMVCA06.BLLMVC
+{
+    class BLLMVCA06
+    {
+
+        private DALMVCA06 DaoBanco = new DALMVCA06();
+        public Boolean Autenticar(string cpf, string nome, string mae)
+        {
+            string consulta = string.Format($"select * from tbl_clientea27 where cpf_cliente = '{cpf}' and nome_cliente = '{nome}' and nome_mae = '{mae}';");
+            DataTable dt = DaoBanco.executarConsulta(consulta);
+
+            if (dt.Rows.Count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
